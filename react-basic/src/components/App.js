cd000dcb47bf654686df2f17a8aab936f88515b6
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-10-25 02:20:17 +0500

    image show/hide on click event using class component

--- react-basic/src/components/App.js
+++ react-basic/src/components/App.js
@@ -12,29 +12,44 @@ import '../assets/css/style.css'
 // }
 
 // hello from functional component
-function app({Name}) {
-    // passing props
-    return (
-        <div className="box">
-            <h1>Hello From {Name}</h1>
-        </div>
-    ) 
-}
+// function app({Name}) {
+//     // passing props
+//     return (
+//         <div className="box">
+//             <h1>Hello From {Name}</h1>
+//         </div>
+//     ) 
+// }
 
 // hello from class component
-// class app extends React.Component {
-//     // passing props
-//     // constructor(props) {
-//     //     super(props);
-//     // }
-//     render() {
-//         return (
-//             <div className="box">
-//                 <h1>Hello From {this.props.Name}</h1>
-//             </div>
-//         )
-//     }
-// }
+
+
+class app extends React.Component {
+    // passing props
+    constructor(props) {
+        super(props);
+        this.state = {
+            showImage: false
+        }
+    }
+    // click handler for toggle image show/hide
+    toggleImage = () => {
+        this.setState({ showImage: !this.state.showImage }) // toggle effect
+        console.log(`State Now ${this.state.showImage}`)
+    }
+    render() {
+        return (
+            <div className="box">
+                <h1>Hello From {this.props.Name}</h1>
+                <button onClick={this.toggleImage} className="toggleBtn">Toggle Image</button>
+                <div>
+                    {this.state.showImage ? <img className="toggleImage" src="https://i.ytimg.com/vi/PwofRNGX3dw/hqdefault.jpg" /> : null}
+
+                </div>
+            </div>
+        )
+    }
+}
 
 
 export default app;
