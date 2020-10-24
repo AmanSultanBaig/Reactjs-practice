423a5355d3e77257c263623144f3befb5a573e62
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-10-25 02:03:28 +0500

    css applied from external css file with both example - funcational - class based

--- react-basic/src/components/App.js
+++ react-basic/src/components/App.js
@@ -1,38 +1,40 @@
 import React from 'react';
+// import css from external css file 
+import '../assets/css/style.css'
 
 // css style object
-let styleObj = {
-    backgroundColor: 'red',
-    padding: '2rem',
-    textAlign: 'center',
-    color: 'white',
-    fontSize: '34px'
-}
-
-// hello from functional component
-// function app({Name}) {
-//     // passing props
-//     return (
-//         <div style={styleObj}>
-//             <h1>Hello From {Name}</h1>
-//         </div>
-//     ) 
+// let styleObj = {
+//     backgroundColor: 'red',
+//     padding: '2rem',
+//     textAlign: 'center',
+//     color: 'white',
+//     fontSize: '34px'
 // }
 
-// hello from class component
-class app extends React.Component {
+// hello from functional component
+function app({Name}) {
     // passing props
-    // constructor(props) {
-    //     super(props);
-    // }
-    render() {
-        return (
-            <div style={styleObj}>
-                <h1>Hello From {this.props.Name}</h1>
-            </div>
-        )
-    }
+    return (
+        <div className="box">
+            <h1>Hello From {Name}</h1>
+        </div>
+    ) 
 }
+
+// hello from class component
+// class app extends React.Component {
+//     // passing props
+//     // constructor(props) {
+//     //     super(props);
+//     // }
+//     render() {
+//         return (
+//             <div className="box">
+//                 <h1>Hello From {this.props.Name}</h1>
+//             </div>
+//         )
+//     }
+// }
 
 
 export default app;
